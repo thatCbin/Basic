4c9a640c1b1b645e41690a91a812cbddce299360
thatCbin <1172816325@qq.com>
2019-12-31 15:38:52 +0800

    MVVM搭建

---
+++ app/src/main/java/com/cb/basic/api/BaseApi.java
@@ -0,0 +1,42 @@
+package com.cb.basic.api;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+/**
+ * create date on 2019/12/31
+ *
+ * @author Cbin
+ * describe 封装基础的Retrofit
+ */
+public abstract class BaseApi {
+
+    /**
+     * 初始化Retrofit
+     */
+    public Retrofit initRetrofit(String baseUrl) {
+        Retrofit.Builder builder = new Retrofit.Builder();
+        //支持返回Call<String>
+        builder.addConverterFactory(ScalarsConverterFactory.create());
+        //支持直接格式化json返回Bean对象
+        builder.addConverterFactory(GsonConverterFactory.create());
+        //支持RxJava
+        builder.addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+        builder.baseUrl(baseUrl);
+        OkHttpClient client = setClient();
+        if (client != null) {
+            builder.client(client);
+        }
+        return builder.build();
+    }
+
+    /**
+     * 设置OkHttpClient，添加拦截器等
+     *
+     * @return 可以返回为null
+     */
+    protected abstract OkHttpClient setClient();
+}
